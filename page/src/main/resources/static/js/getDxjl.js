b5401e55819deef7f10196663683a0b6da3ee0ff
kennyjin601 <yongjin@163.com>
2018-04-18 09:36:21 +0800

    新增

--- page/src/main/resources/static/js/getDxjl.js
+++ page/src/main/resources/static/js/getDxjl.js
@@ -1,9 +1,9 @@
 var dxjlTable;
 function getDxjl(tabid) {
     var rootDiv = $('<div>', {'id': 'dxjlStock' + tabid, 'class': 'l-tab-content-item'});
-    var lhbDiv = $('<div>', {'class': 'l-tab-content-item'});
+    var lhbDiv = $('<div>', {'class': 'l-tab-content-item','style':'overflow-y:scroll'});
     var clearDiv = $('<div>', {'class': 'l-clear'});
-    var maingridDiv = $('<div>', {'id': 'maingridDxjlStock'});
+    var maingridDiv = $('<div>', {'id': 'maingridDxjlStock','class': 'l-tab-content-item','style':'overflow-y:scroll'});
     dxjlTable=$('<table>', {'id': 'tableDxjlStock','width':'100%','border':'1px','cellspacing':'0'});
     maingridDiv.append(dxjlTable);
     lhbDiv.prepend(clearDiv);
